da690c11da5c1f9a6e80d3c4f91f4fed23963c7d
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-10-12 23:13:02 +0100

    added ant design

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,11 @@ module.exports = {
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+    '/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx)$',
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
   testResultsProcessor: 'jest-junit',

a1fed831223d88e671f26ee5b8e95d03aa113d86
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-10-17 00:10:22 +0100

    feat(pages): Challenge page shows different views depending on screen size
    Custom hook 'useWindowSize' added which returns a object containing the window width and height. Thiis is updated when the window is resized. 'useWindowSize' is then used to determine which view to render to the user. Desktop users will have a side-by-side view of the challenge and code editor. Mobile users will only see the code editor and a menu button which opens a drawer containing the challenge data.

--- pages/challenges/[id].tsx
+++ pages/challenges/[id].tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
-import { Drawer, Button, Divider, Typography, Collapse } from 'antd'
+import { useState, useEffect } from 'react'
+import { Drawer, Button, Divider, Typography, Collapse, Row, Col } from 'antd'
+import { MenuUnfoldOutlined } from '@ant-design/icons'
 import CodeEditor from '@/components/CodeEditor'
 
 const { Title, Text } = Typography
@@ -19,47 +20,103 @@ const challengeData = {
   ],
 }
 
-const ChallengePage: NextPage = () => {
-  const [visible, setVisible] = useState(false)
+function useWindowSize(): { width: number; height: number } {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  })
 
-  return (
-    <>
-      <Button type="primary" onClick={() => setVisible(true)}>
-        Open
-      </Button>
-      <CodeEditor />
-      <Drawer
-        width={'75vw'}
-        title={<Title level={3}>{challengeData.title}</Title>}
-        placement="left"
-        onClose={() => setVisible(false)}
-        visible={visible}
-        key={challengeData._id}
-      >
+  function handleResize() {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    })
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize)
+
+      handleResize()
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+  return windowSize
+}
+
+function RenderDetails(title: string, description: string, input: string, output: string, hints?: string[]) {
+  if (!hints) {
+    return (
+      <>
+        <Title level={3}>{title}</Title>
+        <Divider />
         <Title level={4}>Problem Description</Title>
-        <Text>{challengeData.description}</Text>
+        <Text>{description}</Text>
         <Divider />
         <Title level={4}>Input</Title>
-        <Text>{challengeData.input}</Text>
+        <Text>{input}</Text>
         <Divider />
         <Title level={4}>Output</Title>
-        <Text>{challengeData.output}</Text>
+        <Text>{output}</Text>
+      </>
+    )
+  }
+  return (
+    <>
+      <Title level={3}>{title}</Title>
+      <Divider />
+      <Title level={4}>Problem Description</Title>
+      <Text>{description}</Text>
+      <Divider />
+      <Title level={4}>Input</Title>
+      <Text>{input}</Text>
+      <Divider />
+      <Title level={4}>Output</Title>
+      <Text>{output}</Text>
+      <Divider />
+      <Collapse>
+        {hints.map((hint, i) => {
+          return (
+            <Collapse.Panel header={`Hint ${i + 1}`} key={i + 1}>
+              <Text>{hint}</Text>
+            </Collapse.Panel>
+          )
+        })}
+      </Collapse>
+    </>
+  )
+}
 
-        {challengeData.hasOwnProperty('hints') && (
-          <>
-            <Divider />
-            <Collapse>
-              {challengeData.hints.map((hint, i) => {
-                return (
-                  <Collapse.Panel header={`Hint ${i + 1}`} key={i + 1}>
-                    <Text>{hint}</Text>
-                  </Collapse.Panel>
-                )
-              })}
-            </Collapse>
-          </>
-        )}
-      </Drawer>
+const ChallengePage: NextPage = () => {
+  const [visible, setVisible] = useState(false)
+  const size = useWindowSize()
+
+  const { title, description, input, output, hints } = challengeData
+  return (
+    <>
+      <Row>
+        <Col span={size.width > 480 ? 10 : 2}>
+          {size.width > 480 && RenderDetails(title, description, input, output, hints)}
+          {size.width <= 480 && <Button icon={<MenuUnfoldOutlined />} onClick={() => setVisible(true)} />}
+        </Col>
+        <Col span={size.width > 480 ? 14 : 22}>
+          <CodeEditor />
+        </Col>
+      </Row>
+      <>
+        <Drawer
+          width={'75vw'}
+          title={<Title level={3}>{challengeData.title}</Title>}
+          placement="left"
+          onClose={() => setVisible(false)}
+          visible={visible}
+          key={challengeData._id}
+        >
+          {RenderDetails(title, description, input, output, hints)}
+        </Drawer>
+      </>
     </>
   )
 }

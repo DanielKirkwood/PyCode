67b16e0d37c3722a0a8d39c1e6f4fe8fa3bbecc8
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-10-19 21:56:52 +0100

    Updated CodeEditor to output executed code:
    Editor sends code to api route which then makes a request to piston API.
    Had to use api route to hide piston token.
    
    Closes Connect to PistonAPI #8

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { Button, Divider, Typography } from 'antd'
+import { PlayCircleFilled } from '@ant-design/icons'
 import dynamic from 'next/dynamic'
 import styles from '../styles/CodeEditor.module.css'
 
@@ -15,12 +17,51 @@ const CodeMirror = dynamic(
 
 export const CodeEditor = () => {
   const [code, setCode] = useState('')
+  const [output, setOutput] = useState('')
 
   const options = { lineNumbers: true, mode: 'python', theme: 'material', lineWrapping: true }
 
-  const onCodeChange = (code) => setCode(code)
+  const onCodeChange = (code: string) => setCode(code)
 
-  return CodeMirror && <CodeMirror onChange={onCodeChange} options={options} value={code} className={styles.editor} />
+  const executeCode = async (code: string) => {
+    const response = await fetch('/api/executeCode', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ code: code }),
+    })
+    const data = await response.json()
+    console.log(data)
+
+    if (data.error) {
+      setOutput(data.error)
+      console.log('error')
+    } else {
+      setOutput(data.output)
+      console.log('success')
+    }
+  }
+
+  return (
+    CodeMirror && (
+      <>
+        <CodeMirror onChange={onCodeChange} options={options} value={code} className={styles.editor} />
+        <Button
+          onClick={(e) => {
+            e.stopPropagation()
+            executeCode(code)
+          }}
+          shape="round"
+          icon={<PlayCircleFilled />}
+        >
+          Run Code
+        </Button>
+        <Divider />
+        <Typography.Text>{output}</Typography.Text>
+      </>
+    )
+  )
 }
 
 export default CodeEditor

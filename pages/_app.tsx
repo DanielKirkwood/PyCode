d774fe5bd98cd2e7676edbcf2112765da609f915
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-10-30 21:40:45 +0100

    session provider added to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import NavBar from '@/components/NavBar'
 
 import 'antd/dist/antd.css'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
+      <SessionProvider session={session} refetchInterval={5 * 60}>
+        <NavBar />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   )
 }

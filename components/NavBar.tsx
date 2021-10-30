48ec80196580f71f70482dd265360bc0ebf544e2
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-10-30 21:41:35 +0100

    Conditional rendering of navbar and home:
    shows different buttons/links depending on if user is logged in

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,22 +1,53 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { Menu } from 'antd'
-import { CodeOutlined, HomeOutlined, LoginOutlined } from '@ant-design/icons'
+import { FaCode, FaHome } from 'react-icons/fa'
+import { FiLogIn, FiLogOut } from 'react-icons/fi'
+import { CgProfile } from 'react-icons/cg'
 
 function NavBar(): ReactElement {
+  const router = useRouter()
   const [selected, setSelected] = useState('home')
+
+  const { data: session, status } = useSession()
+
+  useEffect(() => {
+    if (router.pathname === '/') {
+      setSelected('home')
+    } else if (router.pathname.startsWith('/challenges')) {
+      setSelected('challenges')
+    } else if (router.pathname === '/login' || router.pathname === '/signup') {
+      setSelected('login')
+    }
+  }, [router])
 
   return (
     <Menu onClick={(e) => setSelected(e.key)} selectedKeys={[selected]} mode="horizontal">
-      <Menu.Item key="home" icon={<HomeOutlined />}>
+      <Menu.Item key="home" icon={<FaHome />}>
         <Link href="/">Home</Link>
       </Menu.Item>
-      <Menu.Item key="challenges" icon={<CodeOutlined />}>
+      <Menu.Item key="challenges" icon={<FaCode />}>
         <Link href={'/challenges'}>Challenges</Link>
       </Menu.Item>
-      <Menu.Item key="login" icon={<LoginOutlined />}>
-        <Link href="/login">Login</Link>
-      </Menu.Item>
+
+      {status === 'unauthenticated' && (
+        <Menu.Item key="login" icon={<FiLogIn />}>
+          <Link href="/login">Login</Link>
+        </Menu.Item>
+      )}
+
+      {status === 'authenticated' && (
+        <>
+          <Menu.Item key="profile" icon={<CgProfile />}>
+            <Link href="/profile">{session.user.name}</Link>
+          </Menu.Item>
+          <Menu.Item key="logout" icon={<FiLogOut />}>
+            <Link href="/logout">Logout</Link>
+          </Menu.Item>
+        </>
+      )}
     </Menu>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import type { NextPage } from 'next'
 import heroCode from '../public/hero-code.png'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
+  const { status } = useSession()
+
   return (
     <div className="pt-24">
       <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
@@ -11,15 +14,28 @@ const Home: NextPage = () => {
           <p className="uppercase tracking-wide w-full">PyCode</p>
           <h1 className="my-4 text-5xl font-bold leading-tight">Develop Your Python Skills</h1>
           <p className="leading-normal text-2xl mb-6">Challenge yourself and others python coding ability</p>
-          <Link href="/">
-            <a
-              data-testid="sign-up button"
-              className="md:mx-0 mx-auto hover:underline hover:text-white bg-blue-400 text-white
+          {status === 'unauthenticated' && (
+            <Link href="/login">
+              <a
+                data-testid="call to action"
+                className="md:mx-0 mx-auto hover:underline hover:text-white bg-blue-400 text-white
              font-bold rounded-lg my-3 py-2 px-4 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-            >
-              Sign-Up
-            </a>
-          </Link>
+              >
+                Login
+              </a>
+            </Link>
+          )}
+          {status === 'authenticated' && (
+            <Link href="/challenges">
+              <a
+                data-testid="call to action"
+                className="md:mx-0 mx-auto hover:underline hover:text-white bg-blue-400 text-white
+             font-bold rounded-lg my-3 py-2 px-4 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Challenges
+              </a>
+            </Link>
+          )}
         </div>
         <div className="flex flex-col w-full md:w-1/2 justify-center justify-items-center ml-10">
           <Image src={heroCode} alt="screenshot of python code" />

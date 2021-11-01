fae3f04f33178e1e73e34cad18dff314c2d5d921
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-11-01 14:59:42 +0000

    Created ChallengeList component:
    displays a section(page) of challenges

---
+++ components/ChallengeList.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ChallengeCard from '@/components/ChallengeCard'
+import useSWR from 'swr'
+
+interface Challenge {
+  _id: number
+  title: string
+  description: string
+}
+
+interface Props {
+  limit: number
+  skip: number
+}
+
+const ChallengeList = ({ limit, skip }: Props) => {
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+  const { data, error } = useSWR(`/api/challenges?limit=${limit}&skip=${skip}`, fetcher)
+
+  return (
+    <div className="py-8 w-full flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full">
+        {error && <h1>Failed to load</h1>}
+        {!data && <h1>Loading...</h1>}
+        {data &&
+          data.map((challenge: Challenge) => (
+            <ChallengeCard
+              id={challenge._id}
+              title={challenge.title}
+              description={challenge.description}
+              key={challenge._id}
+            />
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default ChallengeList

68b3694ac6265ee98362d9b0b75ceda3d6bd88c7
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-11-04 18:17:07 +0000

    Updated create page:
    Page takes user through steps to create challenge

--- pages/challenges/create.tsx
+++ pages/challenges/create.tsx
@@ -4,15 +4,18 @@ import { useSession } from 'next-auth/react'
 import { useRef, useState } from 'react'
 import { Fragment } from 'react'
 import { NextRouter, useRouter } from 'next/router'
+import Stepper from '@/components/Stepper'
 
 const Create: NextPage = () => {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
   const [testCases, setTestCases] = useState([])
+  const [inputs, setInputs] = useState([])
   const [inputName, setInputName] = useState('')
   const [inputValue, setInputValue] = useState('')
   const [output, setOutput] = useState('')
   const [error, setError] = useState('')
+  const [currentStep, setCurrentStep] = useState(1)
 
   const titleInput = useRef(null)
 
@@ -23,6 +26,7 @@ const Create: NextPage = () => {
     e.preventDefault()
     if (!title || !description || !testCases) {
       setError('Please fill out all the fields')
+      return
     }
 
     const response = await fetch('/api/challenges', {
@@ -48,13 +52,24 @@ const Create: NextPage = () => {
     return
   }
 
-  const handleAddTest = () => {
-    const testCase = {
+  const handleAddInput = () => {
+    const newInput = {
       inputName,
       inputValue,
+    }
+    setInputs([...inputs, newInput])
+    setInputName('')
+    setInputValue('')
+    return
+  }
+
+  const handleAddTest = () => {
+    const testCase = {
+      inputs,
       output,
     }
     setTestCases([...testCases, testCase])
+    setInputs([])
     setInputName('')
     setInputValue('')
     setOutput('')
@@ -76,94 +91,157 @@ const Create: NextPage = () => {
         )}
         {status === 'authenticated' && (
           <>
-            <h3 className="text-2xl font-bold text-center">Create challenge</h3>
+            <h3 className="text-2xl font-bold text-center mb-10">Create challenge</h3>
+            <Stepper currentStep={currentStep} steps={['Details', 'Test Cases', 'Review & Confirm']} />
             <form onSubmit={onFormSubmit}>
               <div className="mt-4 w-full">
-                <div>
-                  <label className="block" htmlFor="title">
-                    Title
-                  </label>
-                  <input
-                    id="title"
-                    type="text"
-                    placeholder="My Awesome Challenge"
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                    ref={titleInput}
-                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                  />
-                </div>
-                <div className="mt-4">
-                  <label className="block" htmlFor="description">
-                    Description
-                  </label>
-                  <textarea
-                    id="description"
-                    onChange={(e) => setDescription(e.target.value)}
-                    value={description}
-                    placeholder="Enter the challenge description here"
-                    className="form-textarea px-4 py-2 mt-2 block w-full border rounded-lg focus:outline-none"
-                    rows={4}
-                  />
-                </div>
-                <div className="mt-4">
-                  <h2 className="text-xl font-bold text-center">Test Cases</h2>
-                  <div className="grid grid-cols-12">
-                    <p className="col-span-4 py-2">input name</p>
-                    <p className="col-span-4 py-2">input value</p>
-                    <p className="col-span-4 py-2">output value</p>
+                {currentStep === 1 && (
+                  <>
+                    <div>
+                      <label className="block" htmlFor="title">
+                        Title
+                      </label>
+                      <input
+                        id="title"
+                        type="text"
+                        placeholder="My Awesome Challenge"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                        ref={titleInput}
+                        className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                      />
+                    </div>
+                    <div className="mt-4">
+                      <label className="block" htmlFor="description">
+                        Description
+                      </label>
+                      <textarea
+                        id="description"
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                        placeholder="Enter the challenge description here"
+                        className="form-textarea px-4 py-2 mt-2 block w-full border rounded-lg focus:outline-none"
+                        rows={4}
+                      />
+                    </div>
+                  </>
+                )}
+                {currentStep === 2 && (
+                  <div className="mt-4">
+                    <div className="grid grid-cols-12 gap-4">
+                      <label className="block col-span-6" htmlFor="inputName">
+                        Input name
+                      </label>
+                      <label className="block col-span-6" htmlFor="inputValue">
+                        Input value
+                      </label>
+                      {inputs.map((input, i) => {
+                        return (
+                          <Fragment key={i}>
+                            <p className="block col-span-6">{input.inputName}</p>
+                            <p className="block col-span-6">{input.inputValue}</p>
+                          </Fragment>
+                        )
+                      })}
+                      <input
+                        id="inputName"
+                        type="text"
+                        placeholder="x"
+                        onChange={(e) => setInputName(e.target.value)}
+                        value={inputName}
+                        className="col-span-6 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                      />
+                      <input
+                        id="inputValue"
+                        type="text"
+                        placeholder="5"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        className="col-span-6 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                      />
+                    </div>
+                    <button
+                      onClick={handleAddInput}
+                      type="button"
+                      className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                    >
+                      Add Input
+                    </button>
+
+                    <div className="flex flex-col items-center sm:flex-row sm:items-center justify-between">
+                      <div className="mt-4">
+                        <input
+                          id="output"
+                          type="text"
+                          placeholder="Output"
+                          onChange={(e) => setOutput(e.target.value)}
+                          value={output}
+                          className="col-span-4 sm:col-span-5 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                        />
+                      </div>
+                      <button
+                        onClick={handleAddTest}
+                        type="button"
+                        className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                      >
+                        Confirm test
+                      </button>
+                    </div>
+                  </div>
+                )}
+                {currentStep === 3 && (
+                  <div className="mt-10">
+                    <p className="text-lg my-2">Title: {title}</p>
+                    <p className="text-lg my-2">Description: {description}</p>
                     {testCases.map((test, i) => {
                       return (
                         <Fragment key={i}>
-                          <p className="col-span-4 py2">{test.inputName}</p>
-                          <p className="col-span-4 py2">{test.inputValue}</p>
-                          <p className="col-span-4 py2">{test.output}</p>
+                          <hr className="w-full my-3" />
+                          <p className="text-lg text-center">Test {i + 1}</p>
+                          <p className="text-lg">Inputs:</p>
+                          {test.inputs.map((input, j) => {
+                            return (
+                              <Fragment key={j}>
+                                <p className="text-lg">{`${input.inputName} = ${input.inputValue}`}</p>
+                              </Fragment>
+                            )
+                          })}
+                          <p className="text-lg mt-4">Output: {test.output}</p>
                         </Fragment>
                       )
                     })}
                   </div>
-                  <div className="grid grid-cols-12 gap-4">
-                    <input
-                      id="inputName"
-                      type="text"
-                      placeholder="x"
-                      onChange={(e) => setInputName(e.target.value)}
-                      value={inputName}
-                      className="col-span-3 sm:col-span-2 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                    />
-                    <input
-                      id="inputValue"
-                      type="text"
-                      placeholder="5"
-                      onChange={(e) => setInputValue(e.target.value)}
-                      value={inputValue}
-                      className="col-span-4 sm:col-span-5 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                    />
-                    <input
-                      id="output"
-                      type="text"
-                      placeholder="50"
-                      onChange={(e) => setOutput(e.target.value)}
-                      value={output}
-                      className="col-span-4 sm:col-span-5 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                    />
-                  </div>
-                </div>
+                )}
                 {error && <span className="text-xs tracking-wide text-red-600">{error}</span>}
-                <div className="flex flex-col items-center sm:flex-row sm:items-baseline justify-between">
-                  <button
-                    onClick={handleAddTest}
-                    type="button"
-                    className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
-                  >
-                    Add test case
-                  </button>
-                </div>
                 <hr className="w-full mt-3" />
                 <div className="flex flex-col items-center sm:flex-row sm:items-baseline justify-between">
-                  <button type="submit" className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">
-                    Upload challenge
+                  <button
+                    disabled={currentStep === 1 ? true : false}
+                    onClick={() => setCurrentStep(currentStep - 1)}
+                    type="button"
+                    className={`px-6 py-2 mt-4 text-white rounded-lg ${
+                      currentStep === 1 ? 'bg-gray-500' : 'bg-blue-600 hover:bg-blue-900'
+                    }`}
+                  >
+                    Previous Step
                   </button>
+                  {currentStep !== 3 && (
+                    <button
+                      onClick={() => setCurrentStep(currentStep + 1)}
+                      type="button"
+                      className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                    >
+                      Next Step
+                    </button>
+                  )}
+                  {currentStep === 3 && (
+                    <button
+                      type="submit"
+                      className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                    >
+                      Finish
+                    </button>
+                  )}
                 </div>
               </div>
             </form>

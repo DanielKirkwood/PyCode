65e416a13ca563b643ba90b2c4ffdd41b2f7f334
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-11-11 18:27:42 +0000

    challenge page fetches data from database
    Closes #7

--- pages/challenges/[id].tsx
+++ pages/challenges/[id].tsx
@@ -1,123 +1,61 @@
 import type { NextPage } from 'next'
-import { useState, useEffect } from 'react'
-import { Drawer, Button, Divider, Typography, Collapse, Row, Col } from 'antd'
-import { MenuUnfoldOutlined } from '@ant-design/icons'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+// import useWindowSize from 'lib/helpers/useWindowSize'
 import CodeEditor from '@/components/CodeEditor'
-
-const { Title, Text } = Typography
-
-const challengeData = {
-  _id: 1,
-  title: 'Two Sum',
-  description:
-    'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order.',
-  input: 'nums = [2,7,11,15], target = 9',
-  output: '[0,1]',
-  hints: [
-    "A really brute force way would be to search for all possible pairs of numbers but that would be too slow. Again, it's best to try out brute force solutions for just for completeness. It is from these brute force solutions that you can come up with optimizations.",
-    'So, if we fix one of the numbers, say x, we have to scan the entire array to find the next number y which is value - x where value is the input parameter. Can we change our array somehow so that this search becomes faster?',
-    'The second train of thought is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?',
-  ],
-}
-
-function useWindowSize(): { width: number; height: number } {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  })
-
-  function handleResize() {
-    setWindowSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    })
-  }
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('resize', handleResize)
-
-      handleResize()
-
-      // Remove event listener on cleanup
-      return () => window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-  return windowSize
-}
-
-function RenderDetails(title: string, description: string, input: string, output: string, hints?: string[]) {
-  if (!hints) {
-    return (
-      <>
-        <Title level={3}>{title}</Title>
-        <Divider />
-        <Title level={4}>Problem Description</Title>
-        <Text>{description}</Text>
-        <Divider />
-        <Title level={4}>Input</Title>
-        <Text>{input}</Text>
-        <Divider />
-        <Title level={4}>Output</Title>
-        <Text>{output}</Text>
-      </>
-    )
-  }
-  return (
-    <>
-      <Title level={3}>{title}</Title>
-      <Divider />
-      <Title level={4}>Problem Description</Title>
-      <Text>{description}</Text>
-      <Divider />
-      <Title level={4}>Input</Title>
-      <Text>{input}</Text>
-      <Divider />
-      <Title level={4}>Output</Title>
-      <Text>{output}</Text>
-      <Divider />
-      <Collapse>
-        {hints.map((hint, i) => {
-          return (
-            <Collapse.Panel header={`Hint ${i + 1}`} key={i + 1}>
-              <Text>{hint}</Text>
-            </Collapse.Panel>
-          )
-        })}
-      </Collapse>
-    </>
-  )
-}
+import { Fragment } from 'react'
 
 const ChallengePage: NextPage = () => {
-  const [visible, setVisible] = useState(false)
-  const size = useWindowSize()
+  // const size = useWindowSize()
+  const router = useRouter()
+  const { id } = router.query
 
-  const { title, description, input, output, hints } = challengeData
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+  const { data, error } = useSWR(`/api/challenges/${id}`, fetcher)
+
   return (
-    <>
-      <Row>
-        <Col span={size.width > 480 ? 10 : 2}>
-          {size.width > 480 && RenderDetails(title, description, input, output, hints)}
-          {size.width <= 480 && <Button icon={<MenuUnfoldOutlined />} onClick={() => setVisible(true)} />}
-        </Col>
-        <Col span={size.width > 480 ? 14 : 22}>
-          <CodeEditor />
-        </Col>
-      </Row>
-      <>
-        <Drawer
-          width={'75vw'}
-          title={<Title level={3}>{challengeData.title}</Title>}
-          placement="left"
-          onClose={() => setVisible(false)}
-          visible={visible}
-          key={challengeData._id}
-        >
-          {RenderDetails(title, description, input, output, hints)}
-        </Drawer>
-      </>
-    </>
+    <div className="py-8 w-full flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full">
+        {error && <h1>Failed to load</h1>}
+        {!data && <h1>Loading...</h1>}
+        {data && (
+          <>
+            <h1 className="text-center text-2xl">{data.title}</h1>
+            <div className="grid grid-cols-12 gap-2 sm:gap-5">
+              <div className="text-center col-span-4 md:col-span-6 flex flex-col w-10/12">
+                <div className="flex justify-between items-center mt-3">
+                  <hr className="w-full" />
+                  <span className="p-2 text-gray-400 mb-1">Description</span>
+                  <hr className="w-full" />
+                </div>
+                <div>
+                  <h3 className="text-lg">{data.description}</h3>
+                </div>
+                <div className="flex justify-between items-center mt-3">
+                  <hr className="w-full" />
+                  <span className="p-2 text-gray-400 mb-1">Example</span>
+                  <hr className="w-full" />
+                </div>
+                <div>
+                  <h3 className="text-lg">Inputs:</h3>
+                  {data.testCases[0].inputs.map((input, index) => {
+                    return (
+                      <h3 key={index} className="text-lg">
+                        {input.inputName} = {input.inputValue}
+                      </h3>
+                    )
+                  })}
+                  <h3 className="text-lg">Output: {data.testCases[0].output}</h3>
+                </div>
+              </div>
+              <div className="col-span-8 md:col-span-6">
+                <CodeEditor title={data.title} testCases={data.testCases} />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 

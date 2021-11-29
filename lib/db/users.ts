00855839beaa28f24c2dfa5f5b74d13eadd1c36b
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-11-29 13:06:56 +0000

    Created /users api route:
    route allows users challenge data to be saved and retrieved

---
+++ lib/db/users.ts
@@ -0,0 +1,55 @@
+import { ObjectId } from 'mongodb'
+import { Collection } from 'mongodb'
+
+export const getAllSaved = async (userChallengeData: Collection, userID: string, limit = 50, skip = 0) => {
+  try {
+    const result = await userChallengeData
+      .find({ user: ObjectId.createFromHexString(userID) })
+      .skip(skip)
+      .limit(limit)
+      .toArray()
+    return result
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+export const getSaved = async (userChallengeData: Collection, userID: string, challengeID: string) => {
+  try {
+    if (!ObjectId.isValid(userID) || !ObjectId.isValid(challengeID)) {
+      return { error: 'either user id or challenge id is invalid' }
+    }
+    const result = await userChallengeData.findOne({
+      user: ObjectId.createFromHexString(userID),
+      challenge: ObjectId.createFromHexString(challengeID),
+    })
+    return result
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+export const SaveOne = async (userChallengeData: Collection, userID: string, challengeID: string, code: string) => {
+  try {
+    const result = await userChallengeData.updateOne(
+      {
+        user: ObjectId.createFromHexString(userID),
+        challenge: ObjectId.createFromHexString(challengeID),
+      },
+      {
+        $set: {
+          user: ObjectId.createFromHexString(userID),
+          challenge: ObjectId.createFromHexString(challengeID),
+          code: code,
+        },
+      },
+      { upsert: true }
+    )
+    return result.upsertedId
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
---
+++ pages/api/[...slug].tsx
@@ -0,0 +1,44 @@
+import { getSaved, SaveOne } from 'lib/db/users'
+import clientPromise from 'lib/db/mongodb'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const dbClient = await clientPromise
+  const userChallengeData = dbClient.db(process.env.MONGODB_DB).collection('user_challenge_data')
+
+  const {
+    query: { slug },
+    method,
+  } = req
+
+  if (slug.length !== 2) {
+    res.status(500).json({ error: 'userID or challengeID not provided' })
+  }
+  const userID = slug[0]
+  const challengeID = slug[1]
+
+  switch (method) {
+    case 'GET':
+      const document = await getSaved(userChallengeData, userID.toString(), challengeID.toString())
+      if (document === null) {
+        res.status(500).json({ error: `error getting saved document` })
+        return
+      }
+      res.status(200).json(document)
+      break
+    case 'PATCH':
+      const result = await SaveOne(userChallengeData, userID.toString(), challengeID.toString(), req.body.code)
+      if (result === null) {
+        res.status(500).json({ error: 'error saving' })
+        return
+      }
+      res.status(200).json({ message: `challenge with id ${challengeID.toString} successfully saved` })
+      break
+    default:
+      if (process.env.NODE_ENV === 'production') {
+        dbClient.close()
+      }
+      res.setHeader('Allow', ['GET', 'PATCH'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}

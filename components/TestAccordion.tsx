59cee6464f28289d5a027f69e8ab9c89ad4fa21a
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-12-21 22:24:28 +0000

    Linting: Add linter functionality Fixes #14
    New api route '/api/code' created which accepts POST request.
    The request requires a parameter execute which when set to true executes the code. If set to false then the code is linted.

--- components/TestAccordion.tsx
+++ components/TestAccordion.tsx
@@ -34,7 +34,7 @@ const TestAccordion = ({ code, inputs, output, testNumber, fnName }: Props) => {
 
     const codeToExecute = `${code}\nassert ${fnName}(${inputValues}) == ${output}`
 
-    const response = await fetch('/api/executeCode', {
+    const response = await fetch('/api/code/execute', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

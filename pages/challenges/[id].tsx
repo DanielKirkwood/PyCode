2ee27ae97f01348b7f069db1d4f34ef562e8e87b
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2021-12-26 13:15:06 +0000

    challenge page updated to display link back to challenges page if error fetching challenge

--- pages/challenges/[id].tsx
+++ pages/challenges/[id].tsx
@@ -1,29 +1,56 @@
-import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
-// import useWindowSize from 'lib/helpers/useWindowSize'
+import type { GetServerSideProps, NextPage } from 'next'
 import CodeEditor from '@/components/CodeEditor'
 import { CommentList } from '@/components/CommentList'
 import { useSession } from 'next-auth/react'
+import { getOne } from 'lib/db/challenges'
+import clientPromise from 'lib/db/mongodb'
+import Link from 'next/link'
 
-const ChallengePage: NextPage = () => {
-  // const size = useWindowSize()
-  const router = useRouter()
-  const { id } = router.query
+interface TestCase {
+  inputs: {
+    inputName: string
+    inputValue: string
+  }[]
+  output: string
+}
+interface challengeData {
+  id: string
+  title: string
+  owner: string
+  description: string
+  verified: boolean
+  testCases: TestCase[]
+  createdAt: string
+}
 
-  const fetcher = (url) => fetch(url).then((res) => res.json())
-  const { data, error } = useSWR(`/api/challenges/${id}`, fetcher)
+interface Props {
+  challenge: challengeData
+}
 
+const ChallengePage: NextPage<Props> = ({ challenge }) => {
   const { data: session } = useSession()
 
   return (
     <div className="py-8 w-full flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full">
-        {error && <h1>Failed to load</h1>}
-        {!data && <h1>Loading...</h1>}
-        {data && (
+        {!challenge && (
+          <div className="text-center">
+            <h1 className="text-2xl mb-6">
+              There was an error getting this challenge. Ensure the url is correct or...
+            </h1>
+            <Link href="/challenges">
+              <a
+                className="px-6 py-2 mt-4  bg-blue-600 rounded-lg hover:bg-blue-900 hover:underline text-white
+               font-bold my-3  shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Go to challenges page
+              </a>
+            </Link>
+          </div>
+        )}
+        {challenge && (
           <>
-            <h1 className="text-center text-2xl">{data.challenge.title}</h1>
+            <h1 className="text-center text-2xl">{challenge.title}</h1>
             <div className="grid grid-cols-12 gap-2 sm:gap-5">
               <div className="text-center col-span-4 md:col-span-6 flex flex-col w-10/12">
                 <div className="flex justify-between items-center mt-3">
@@ -32,7 +59,7 @@ const ChallengePage: NextPage = () => {
                   <hr className="w-full" />
                 </div>
                 <div>
-                  <h3 className="text-lg">{data.challenge.description}</h3>
+                  <h3 className="text-lg">{challenge.description}</h3>
                 </div>
                 <div className="flex justify-between items-center mt-3">
                   <hr className="w-full" />
@@ -41,30 +68,60 @@ const ChallengePage: NextPage = () => {
                 </div>
                 <div>
                   <h3 className="text-lg">Inputs:</h3>
-                  {data.challenge.testCases[0].inputs.map((input, index) => {
+                  {challenge.testCases[0].inputs.map((input, index) => {
                     return (
                       <h3 key={index} className="text-lg">
                         {input.inputName} = {input.inputValue}
                       </h3>
                     )
                   })}
-                  <h3 className="text-lg">Output: {data.challenge.testCases[0].output}</h3>
+                  <h3 className="text-lg">Output: {challenge.testCases[0].output}</h3>
                 </div>
               </div>
               <div className="col-span-8 md:col-span-6">
-                <CodeEditor title={data.challenge.title} testCases={data.challenge.testCases} challengeID={id} />
+                <CodeEditor title={challenge.title} testCases={challenge.testCases} challengeID={challenge.id} />
               </div>
             </div>
             {(session?.user.role === 'admin' ||
               session?.user.role === 'super-admin' ||
-              data.challenge.owner === session?.user.id) && (
-              <CommentList challengeID={id} isVerified={data.challenge.verified} />
+              challenge.owner === session?.user.id) && (
+              <CommentList challengeID={challenge.id} isVerified={challenge.verified} />
             )}
           </>
         )}
       </div>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // get challenge data from database
+  // this function will be called server-side and therefore will not be exposed to client
+  const dbClient = await clientPromise
+  const challenges = dbClient.db(process.env.MONGODB_DB).collection('challenges')
+
+  const document = await getOne(challenges, context.params.id.toString())
+  const challengeData = JSON.parse(JSON.stringify(document))
+
+  if (challengeData.error) {
+    return {
+      props: {},
+    }
+  }
+
+  return {
+    props: {
+      challenge: {
+        id: challengeData._id,
+        title: challengeData.title,
+        description: challengeData.description,
+        testCases: challengeData.testCases,
+        owner: challengeData.owner,
+        verified: challengeData.verified,
+        createdAt: challengeData.createdAt,
+      },
+    },
+  }
 }
 
 export default ChallengePage

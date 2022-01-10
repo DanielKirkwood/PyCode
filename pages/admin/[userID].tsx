c531b6cffca593747aa9082a48d59ccc7a69b71f
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2022-01-09 23:16:25 -0800

    Updated /admin/[userID] page to use Table component

--- pages/admin/[userID].tsx
+++ pages/admin/[userID].tsx
@@ -4,7 +4,7 @@ import { getUser } from 'lib/db/users'
 import type { GetServerSideProps, NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
-import { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import useSWR from 'swr'
 
 interface Props {
@@ -23,67 +23,152 @@ interface Props {
 const UserAdmin: NextPage<Props> = ({ user }) => {
   // get session to determine if user already signed in
   const { data: session, status } = useSession()
-  const loading = status === 'loading'
 
-  // if (typeof window !== 'undefined' && loading) return null
+  const [challengeToEdit, setChallengeToEdit] = useState(null)
+  const [editFormData, setEditFormData] = useState({
+    title: '',
+    description: '',
+    verified: false,
+  })
 
   const [limit, setLimit] = useState(Number(20))
   const [skip, setSkip] = useState(Number(0))
+  const [searchName, setSearchName] = useState('')
 
   const fetcher = (url: RequestInfo) => fetch(url).then((res) => res.json())
   const { data, error, mutate } = useSWR(`/api/challenges?user=${user._id}&limit=${limit}&skip=${skip}`, fetcher)
+  console.log(data)
+
+  const handleSearchNameChange = (event) => {
+    const searchNameValue = event.target.value
+    setSearchName(searchNameValue)
+  }
+
+  const handleLimitChange = (event) => {
+    setLimit(Number(event.target.value))
+  }
+
+  const handleEditClick = (event, challenge) => {
+    setChallengeToEdit(challenge._id)
+
+    const formValues = {
+      title: challenge.title,
+      description: challenge.description,
+      verified: challenge.verified,
+    }
+    setEditFormData(formValues)
+  }
+
+  const handleEditOnChange = (event) => {
+    const fieldName = event.target.getAttribute('name')
+    const fieldValue = fieldName === 'verified' ? event.target.checked : event.target.value
+
+    const newFormData = { ...editFormData }
+    newFormData[fieldName] = fieldValue
+
+    setEditFormData(newFormData)
+  }
+
+  const handleCancelClick = () => {
+    setEditFormData({
+      title: '',
+      description: '',
+      verified: false,
+    })
+    setChallengeToEdit(null)
+  }
 
   return (
     <div className="pt-24 w-full">
-      <div className="container  mx-auto px-3 flex flex-wrap flex-col md:flex-row items-center">
-        {status === 'unauthenticated' && (
-          <div className="text-center">
-            <p className="py-2 mb-4 text-center text-md tracking-wide text-red-600">
-              You must be logged in to view the admin page
-            </p>
-            <Link href="/login">
-              <a className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</a>
-            </Link>
-          </div>
-        )}
-        {status === 'authenticated' && session?.user.role !== 'super-admin' && (
-          <div className="text-center">
-            <p className="py-2 mb-4 text-center text-md tracking-wide text-red-600">
-              Only users with the role of &quot;super-admin&quot; can view this page
-            </p>
-            <Link href="/challenges">
-              <a className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Go to challenges page</a>
-            </Link>
-          </div>
-        )}
-        {status === 'authenticated' && session?.user.role === 'super-admin' && (
-          <section className="text-gray-600 body-font w-full">
-            <div className="container flex flex-wrap px-5 py-24 mx-auto items-center">
-              <div className="md:w-1/4 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
-                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-                  {user.name}{' '}
-                  <span className="px-4 py-1  rounded-full bg-green-100 text-green-800  font-semibold text-sm">
-                    {user.role}
-                  </span>
-                </h1>
-                <p className="leading-relaxed text-base">{user.email}</p>
-              </div>
-              <div className="flex flex-col md:w-3/4 md:pl-12">
-                <h2 className="title-font font-semibold text-gray-800 tracking-wider text-sm mb-3 uppercase">
-                  Challenges
-                </h2>
-                <div className="block w-full overflow-x-auto">
-                  <Table editable headings={['id', 'name', 'email', 'role']}>
-                    <Table.Row data={{ _id: '1234', name: 'Daniel', email: 'user@email.com', role: 'super-admin' }} />
-                    <Table.Row data={{ _id: '5678', name: 'John', email: 'user2@email.com', role: 'admin' }} />
-                    <Table.Row data={{ _id: '1356', name: 'Dillon', email: 'user3@email.com', role: 'user' }} />
-                  </Table>
-                </div>
-              </div>
+      {status === 'unauthenticated' && (
+        <div className="text-center">
+          <p className="py-2 mb-4 text-center text-md tracking-wide text-red-600">
+            You must be logged in to view the admin page
+          </p>
+          <Link href="/login">
+            <a className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</a>
+          </Link>
+        </div>
+      )}
+      {status === 'authenticated' && session?.user.role !== 'super-admin' && (
+        <div className="text-center">
+          <p className="py-2 mb-4 text-center text-md tracking-wide text-red-600">
+            Only users with the role of &quot;super-admin&quot; can view this page
+          </p>
+          <Link href="/challenges">
+            <a className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Go to challenges page</a>
+          </Link>
+        </div>
+      )}
+      {status === 'authenticated' && session?.user.role === 'super-admin' && (
+        <div>
+          <h1>Admin Dashboard</h1>
+          {error && <h1>There was an error</h1>}
+          {!data && <h1>Loading user data</h1>}
+          <div className="my-2 flex sm:flex-row flex-col">
+            <div className="block relative">
+              <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg viewBox="0 0 24 24" className="h-4 w-4 fill-current text-gray-500">
+                  <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
+                </svg>
+              </span>
+              <input
+                onChange={(event) => handleSearchNameChange(event)}
+                value={searchName}
+                name="searchName"
+                id="searchName"
+                placeholder="Search for a name..."
+                className="appearance-none rounded-l sm:rounded-r-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+              />
             </div>
-          </section>
-        )}
-      </div>
+            <div className="relative">
+              <select
+                onChange={(event) => handleLimitChange(event)}
+                value={limit}
+                className="h-full rounded-r border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              >
+                <option value={5}>Show 5 results</option>
+                <option value={10}>Show 10 results</option>
+                <option value={20}>Show 20 results</option>
+                <option value={50}>Show 50 results</option>
+              </select>
+            </div>
+          </div>
+          <Table
+            editable
+            // handleEditSubmit={onFormSubmit}
+            handleEditClick={handleEditClick}
+            headings={['_id', 'title', 'owner', 'verified']}
+            protectedNames={['_id', 'owner']}
+          >
+            {data &&
+              data.payload.challenges.map((challenge) => (
+                <Fragment key={challenge._id}>
+                  {challengeToEdit === challenge._id ? (
+                    <Table.EditableRow
+                      data={challenge}
+                      editFormData={editFormData}
+                      handleCancelClick={handleCancelClick}
+                      handleEditOnChange={handleEditOnChange}
+                    />
+                  ) : (
+                    <Table.Row data={challenge} />
+                  )}
+                </Fragment>
+              ))}
+            {data && (
+              <Table.Pagination
+                limit={limit}
+                skip={skip}
+                setSkip={setSkip}
+                numRows={data.payload.numDocuments}
+                totalRows={data.payload.totalDocuments}
+                numRowsRemaining={data.payload.numDocumentsRemaining}
+              />
+            )}
+          </Table>
+        </div>
+      )}
     </div>
   )
 }

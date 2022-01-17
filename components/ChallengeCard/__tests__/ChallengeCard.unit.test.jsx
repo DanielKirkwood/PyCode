2801fa9f8fd214f3dfa7897c82b6985ad293ba17
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2022-01-17 00:42:29 +0000

    added ChallengeList tests

--- components/ChallengeCard/__tests__/ChallengeCard.unit.test.jsx
+++ components/ChallengeCard/__tests__/ChallengeCard.unit.test.jsx
@@ -8,22 +8,24 @@ jest.mock('next/link', () => {
   return ({ children, href }) => React.cloneElement(React.Children.only(children), { href })
 })
 
-test('should display a challenges details ', () => {
-  const fakeChallenge = {
-    id: 12345678,
-    title: 'My Fake Challenge Title',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eros donec ac odio tempor. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Eget duis at tellus at urna condimentum. Aliquam nulla facilisi cras fermentum odio eu feugiat. Amet nisl suscipit adipiscing bibendum. Netus et malesuada fames ac.',
-  }
-  const { getByText } = render(
-    <ChallengeCard id={fakeChallenge.id} title={fakeChallenge.title} description={fakeChallenge.description} />
-  )
+describe('ChallengeCard', () => {
+  test('should display a challenges details ', () => {
+    const fakeChallenge = {
+      id: 12345678,
+      title: 'My Fake Challenge Title',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eros donec ac odio tempor. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Eget duis at tellus at urna condimentum. Aliquam nulla facilisi cras fermentum odio eu feugiat. Amet nisl suscipit adipiscing bibendum. Netus et malesuada fames ac.',
+    }
+    const { getByText } = render(
+      <ChallengeCard id={fakeChallenge.id} title={fakeChallenge.title} description={fakeChallenge.description} />
+    )
 
-  const titleNode = getByText(fakeChallenge.title)
-  const descriptionNode = getByText(fakeChallenge.description)
-  const challengePageAnchorNode = getByText('Try Challenge')
+    const titleNode = getByText(fakeChallenge.title)
+    const descriptionNode = getByText(fakeChallenge.description)
+    const challengePageAnchorNode = getByText('Try Challenge')
 
-  expect(titleNode).toHaveTextContent(fakeChallenge.title)
-  expect(descriptionNode).toHaveTextContent(fakeChallenge.description)
-  expect(challengePageAnchorNode).toHaveAttribute('href', `/challenges/${fakeChallenge.id}`)
+    expect(titleNode).toHaveTextContent(fakeChallenge.title)
+    expect(descriptionNode).toHaveTextContent(fakeChallenge.description)
+    expect(challengePageAnchorNode).toHaveAttribute('href', `/challenges/${fakeChallenge.id}`)
+  })
 })
--- pages/challenges/index.tsx
+++ pages/challenges/index.tsx
@@ -1,4 +1,4 @@
-import ChallengeList from '@/components/ChallengeList'
+import ChallengeList from '@/components/ChallengeList/ChallengeList'
 import Pagination from '@/components/Pagination'
 import SkeletonChallengeList from '@/components/SkeletonChallengeList'
 import type { NextPage } from 'next'
@@ -9,7 +9,7 @@ const Challenges: NextPage = () => {
   const [limit] = useState<number>(20)
   const [skip, setSkip] = useState<number>(0)
 
-  const fetcher = (url) => fetch(url).then((res) => res.json())
+  const fetcher = (url: string) => fetch(url).then((res) => res.json())
   const { data, error } = useSWR(`/api/challenges?limit=${limit}&skip=${skip}`, fetcher)
 
   function onSkipBtnClick(n: number): void {
